98526c7c859a55ff0f24502c8b1a7b0d581d23d6
HoangNam <namlnhtv00341@gmail.com>
2026-06-07 17:02:09 +0700

    update

--- src/main/java/org/example/bookinghotels/BookinghotelsApplication.java
+++ src/main/java/org/example/bookinghotels/BookinghotelsApplication.java
@@ -2,12 +2,13 @@ package org.example.bookinghotels;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.security.autoconfigure.SecurityAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
 public class BookinghotelsApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BookinghotelsApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BookinghotelsApplication.class, args);
+    }
 
 }
