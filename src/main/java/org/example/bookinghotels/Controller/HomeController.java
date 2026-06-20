75966d2a6e6e1dc796e1abdfea2eb21e9a7c5514
HoangNam <namlnhtv00341@gmail.com>
2026-06-20 15:51:22 +0700

    nâng cấp ui booking cơ bản

--- src/main/java/org/example/bookinghotels/Controller/HomeController.java
+++ src/main/java/org/example/bookinghotels/Controller/HomeController.java
@@ -1,7 +1,19 @@
 package org.example.bookinghotels.Controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+
+@Controller
 public class HomeController {
 
+    @GetMapping({"/", "/home"})
+    public String home() {
+        return "layout.html";
     }
+
+    @GetMapping("/home/layout")
+    public String layout() {
+        return "layout.html";
+    }
+}
