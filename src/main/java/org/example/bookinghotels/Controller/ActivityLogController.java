c82161bd244d2db6a471ae92b1512865e1824c2c
phatndtv00278-bit <phatndtv00278@gmail.com>
2026-08-25 20:38:49 +0700

    Fix ActivityLogController merge conflict

--- src/main/java/org/example/bookinghotels/Controller/ActivityLogController.java
+++ src/main/java/org/example/bookinghotels/Controller/ActivityLogController.java
@@ -7,8 +7,6 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/admin")
 @RequiredArgsConstructor
@@ -21,22 +19,10 @@ public class ActivityLogController {
 
         var logs = systemManagementService.getAllLogs();
 
-<<<<<<< HEAD
-        Page<ActivityLog> result = systemManagementService.searchLogs(
-                keyword,
-                action,
-                module,
-                fromDate,
-                toDate,
-                pageable
-        );
-=======
         System.out.println("SO LOG = " + logs.size());
->>>>>>> f39808f342c6a3f7c62a202f763f3e18dc5d14d9
 
         model.addAttribute("logs", logs);
 
         return "html/admin-html/logs";
     }
-
 }
